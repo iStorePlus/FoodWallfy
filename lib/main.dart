fe6585d49bf22b07757fb5de142543ced8643627
Parth Aggarwal <frazile.com@gmail.com>
2020-02-17 14:50:58 +0530

    firebase crashlytics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:foodwallfy/constants/frazile.dart';
@@ -23,6 +24,15 @@ void main() {
 
   // * Get Shared Preference Instance for whole app
   // Frazile.prefs = await SharedPreferences.getInstance();
+
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(ConfigPage());
 }
